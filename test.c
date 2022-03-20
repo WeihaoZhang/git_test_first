eb2c2921e82ec95ce1ff7f52c1e1f464f8fc0ddf
WeihaoZhang <18217329156@163.com>
2022-03-20 05:23:18 -0700

    add makefile

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+	char *buf = (char*)malloc(4096);
+	memset(buf,0,100*4096);
+	printf("buffer address = 0x%p\n",buf);
+	free(buf);
+	return 0;
+}
